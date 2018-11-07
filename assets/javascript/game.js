dd045b94a735eb875adef399f0040dfe056216ac
Austin Sharman <asharman22@gmail.com>
2018-11-07 17:03:21 -0500

    create game object and fill with data

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,59 +2,59 @@ var currentWordDisplay = document.getElementById("current-word-display");
 var guessesLeftDisplay = document.getElementById("guesses-left-display");
 var guessedDisplay = document.getElementById("guessed-display");
 
-
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var wordBank = ["espresso", "pour over", "chemex"];
-var guessedLetters = [];
-
-function pullWord() {
+var game = {
+    alphabet: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+    wordBank: ["espresso", "pour over", "chemex"],
+    guessedLetters: [],
+    currentWord: "",
+    hiddenWord: "",
     // Select random word from wordBank and assign it to current word
-    var currentWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-    return currentWord;
-}
-
-//Splits the current word up into an array of each character
-// Loop over the selected word and fill an array with each letter
-function wordSplit(currentWord) {
-    var currentWordArray = [];
-
-    for (let i = 0; i < currentWord.length; i++) {
-        currentWordArray.push(currentWord.charAt(i));
-    }
-    var hiddenWord = hideWord(currentWordArray);
-    return hiddenWord;
-}
-
-function hideWord(currentWordArray) {
-    var hiddenWord = "";
-    for (i = 0; i < currentWordArray.length; i++) {
-        if (currentWordArray[i] === " ") {
-            hiddenWord += " ";
-        } else {
-            hiddenWord += "_ ";
+    pullWord: function () {
+        this.currentWord = this.wordBank[Math.floor(Math.random() * this.wordBank.length)];
+        this.wordSplit(this.currentWord);
+    },
+    //Splits the current word up into an array of each character
+    wordSplit: function (word) {
+        var currentWordArray = [];
+        for (i = 0; i < word.length; i++) {
+            currentWordArray.push(word.charAt(i));
         }
-    }
-    return hiddenWord;
+        this.hideWord(currentWordArray);
+    },
+    // Creates a string that replaces every element in an array with an underscore
+    hideWord: function (array) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === " ") {
+                this.hiddenWord += "  ";
+            } else {
+                this.hiddenWord += "_ ";
+            }
+        }
+    },
 }
 
-document.onkeyup = function(event) {
-    var input = event.key.toLocaleLowerCase();
+// document.onkeyup = function (event) {
+//     var input = event.key.toLocaleLowerCase();
 
-    if (alphabet.indexOf(input) >= 0) {
-        console.log(`You typed an appropriate character!`);
-        if (guessedLetters.indexOf(input) == -1) {
-            
-        }        
-    } else {
-        console.log(`Character is not valid...`);
-        
-    }
-}
+//     if (alphabet.indexOf(input) >= 0) {
+//         console.log(`You typed an appropriate character!`);
+//         if (guessedLetters.indexOf(input) == -1) {
+
+//         }
+//     } else {
+//         console.log(`Character is not valid...`);
+
+//     }
+// }
+
+game.pullWord();
+console.log(`${game.currentWord}`);
+console.log(`${game.hiddenWord}`);
 
 
-var currentWord = pullWord();
-var hiddenWord = wordSplit(currentWord); 
-console.log(`${currentWord}`);
-console.log(`${hiddenWord}`);
+// var currentWord = pullWord();
+// var hiddenWord = wordSplit(currentWord);
+// console.log(`${currentWord}`);
+// console.log(`${hiddenWord}`);
 
 
