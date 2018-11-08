1b9ff14d0b06eb3ab45cda4bd48f11ace6bb7d82
Austin Sharman <asharman22@gmail.com>
2018-11-07 20:28:33 -0500

    Add if else statements to check the user's input and see if the letter is correct or not

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,8 @@ var currentWordDisplay = document.getElementById("current-word-display");
 var guessesLeftDisplay = document.getElementById("guesses-left-display");
 var guessedDisplay = document.getElementById("guessed-display");
 
+var guessedLetters = [];
+
 var game = {
     alphabet: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
     wordBank: ["espresso", "pour over", "chemex"],
@@ -31,27 +33,49 @@ var game = {
                 this.hiddenWord += "_ ";
             }
         }
+
+        // Deletes every space element in currentWordArray so the game doesn't expect you to guess the space
+        for (let i = array.length - 1; i >= 0, i--;) {
+            if (array[i] === " ") {
+                array.splice(i, 1);
+            }
+        }
+        this.currentWordArray = array;
     },
 }
 
-// document.onkeyup = function (event) {
-//     var input = event.key.toLocaleLowerCase();
-
-//     if (alphabet.indexOf(input) >= 0) {
-//         console.log(`You typed an appropriate character!`);
-//         if (guessedLetters.indexOf(input) == -1) {
-
-//         }
-//     } else {
-//         console.log(`Character is not valid...`);
-
-//     }
-// }
 
 game.pullWord();
 console.log(`${game.currentWord}`);
 console.log(`${game.currentWordArray}`);
 console.log(`${game.hiddenWord}`);
+
+document.onkeyup = function (event) {
+    var input = event.key.toLowerCase();
+
+
+    if (game.alphabet.indexOf(input) >= 0) {
+        console.log(`You typed an appropriate character!`);
+        if (guessedLetters.indexOf(input) == -1) {
+            console.log(`You haven't guessed this letter!`);
+            if (game.currentWordArray.indexOf(input) >= 0) {
+                console.log(`This letter is in your word!`);
+
+            } else {
+                console.log(`This letter is not in your word`);
+                guessedLetters.push(input);
+            }
+
+        } else {
+            console.log(`You've already guessed this letter`);
+        }
+    } else {
+        console.log(`Character is not valid...`);
+
+    }
+}
+
+
 
 
 
